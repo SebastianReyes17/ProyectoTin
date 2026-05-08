a3e714c325dc916d6bf85969d2ca5992ed32b1c4
SebastianReyes17 <jhoan.reyes@correounivalle.edu.co>
2026-05-08 12:46:03 -0500

    AppTin.js

--- app.js
+++ app.js
@@ -1,54 +1,73 @@
-"use strict";
 const g = 9.81;
+
 // Factorial
-function factorialCalc(n) {
-    let f = 1;
-    for (let i = 1; i <= n; i++) {
-        f *= i;
+function f(n){
+
+    let r = 1;
+
+    for(let i = 1; i <= n; i++){
+
+        r *= i;
     }
-    return f;
+
+    return r;
 }
-// Seno con Taylor
-function senoTaylor(x, n) {
-    let resultado = 0;
-    let signo = 1;
-    for (let i = 0; i < n; i++) {
-        let potencia = Math.pow(x, 2 * i + 1);
-        let factorial = factorialCalc(2 * i + 1);
-        resultado += signo * (potencia / factorial);
-        signo *= -1;
+
+// Taylor
+function seno(x,n){
+
+    let s = 0;
+
+    for(let i = 0; i < n; i++){
+
+        s += ((-1)**i)*(x**(2*i+1))/f(2*i+1);
     }
-    return resultado;
+
+    return s;
 }
-// Método de Euler
-function euler(theta0, omega0, L, h, tiempo, nTaylor) {
+
+// Euler
+function ejecutar(){
+
+    let theta = parseFloat(theta0.value);
+
+    let omega = parseFloat(omega0.value);
+
+    let L = parseFloat(L1.value);
+
+    let h = parseFloat(h1.value);
+
+    let tiempo = parseFloat(tiempo1.value);
+
+    let n = parseInt(nTaylor.value);
+
+    tabla.innerHTML = "";
+
     let t = 0;
-    let theta = theta0;
-    let omega = omega0;
-    let resultados = [];
-    while (t <= tiempo) {
-        resultados.push({ t, theta, omega });
-        let dtheta = omega;
-        let domega = -(g / L) * senoTaylor(theta, nTaylor);
-        theta = theta + h * dtheta;
-        omega = omega + h * domega;
+
+    while(t <= tiempo){
+
+        let error =
+        Math.abs(Math.sin(theta)-seno(theta,n));
+
+        tabla.innerHTML += `
+        <tr>
+            <td>${t.toFixed(2)}</td>
+            <td>${theta.toFixed(4)}</td>
+            <td>${omega.toFixed(4)}</td>
+            <td>${error.toFixed(8)}</td>
+        </tr>
+        `;
+
+        omega =
+        omega + h * (-(g/L) * seno(theta,n));
+
+        theta =
+        theta + h * omega;
+
         t += h;
     }
-    return resultados;
-}
-// Ejecutar simulación
-function ejecutar() {
-    let datos = euler(0.5, // θ inicial
-    0, // ω inicial
-    1, // longitud
-    0.1, // paso
-    5, // tiempo
-    5 // Taylor
-    );
-    let texto = "";
-    // 🔥 SOLO 5 RESULTADOS
-    datos.slice(0, 5).forEach(d => {
-        texto += `t=${d.t.toFixed(2)} | θ=${d.theta.toFixed(4)} | ω=${d.omega.toFixed(4)}\n`;
-    });
-    document.getElementById("output").textContent = texto;
+
+    redondeo.innerHTML =
+    "0.1 + 0.2 = " + (0.1 + 0.2);
 }
